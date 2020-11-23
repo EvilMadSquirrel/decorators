9856df34fd8b9b0fa799a9205702ff399a0894e6
EvilMadSquirrel <minichev.s.l@gmail.com>
2020-11-23 15:31:55 +0300

    decorator factory

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,16 @@
-console.log("hello!");
+function Logger(message: string) {
+  return function (constructor: Function) {
+    console.log(message);
+    console.log(constructor);
+  };
+}
+
+@Logger("Logging Person")
+class Person {
+  name = "Serega";
+  constructor() {
+    console.log("Creating a Person object...");
+  }
+}
+
+const p1 = new Person();
