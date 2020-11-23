35c83440a74a92483f6bf3407f71960d0fbdeb23
EvilMadSquirrel <minichev.s.l@gmail.com>
2020-11-23 16:07:00 +0300

    multiple decorators

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,21 @@
-function WithTemplate(template: string, hookID: string) {
+function Logger(message: string) {
+  console.log("Started logger");
   return function (_: Function) {
+    console.log(message);
+  };
+}
+
+function WithTemplate(template: string, hookID: string) {
+  console.log("Started template maker");
+  return function (_: Function) {
+    console.log("Working with template");
     const el = document.querySelector(`#${hookID}`);
     if (el) {
       el.innerHTML = template;
     }
   };
 }
+@Logger("Logging")
 @WithTemplate("<h1>Hello from Decorator</h1>", "app")
 class Person {
   name = "Serega";
